b1aa1e9fc0fe8e163d74961b4bc7c8b9160b233a
darwinbaide <dajers96@yahoo.com>
2020-07-08 13:23:42 -0400

    starting functions

--- requests.php
+++ requests.php
@@ -1,38 +1,40 @@
 <?php
-echo "At start";
-
-
-//$xml = file_get_contents("http://192.168.1.212/api/EwxOVEswAQTG-VfGrAYqbUHmGbCAm3sKDOOC-Uz2/lights/7");
-//var_dump($xml);
-
-
-/* //The url you wish to send the POST request to
-$url = "http://192.168.1.212//api/EwxOVEswAQTG-VfGrAYqbUHmGbCAm3sKDOOC-Uz2/lights/7/state";
-
-//The data you want to send via POST
-$fields = [
-    'on'      => "true",
-    'bri' => "254",
-    'sat'         => '254'
-]; */
 
 
 
+$xml = file_get_contents("http://192.168.1.212/api/EwxOVEswAQTG-VfGrAYqbUHmGbCAm3sKDOOC-Uz2/groups");
+$data= json_decode($xml);
+//$room= $data->{"4"};
 
-//The URL that we want to send a PUT request to.
-$url = 'http://192.168.1.212/api/EwxOVEswAQTG-VfGrAYqbUHmGbCAm3sKDOOC-Uz2/lights/7/state';
+//var_dump($data);
+//var_dump($room);
+
+
+foreach ($data as $group){
+    echo "\n".($group->name);
+
+}
+
+
+
+//$xml = file_get_contents("http://192.168.1.212/api/EwxOVEswAQTG-VfGrAYqbUHmGbCAm3sKDOOC-Uz2/lights");
+//$data= json_decode($xml);
+//var_dump($data);
  
 $arrData['on'] = true;
 $arrData['sat'] = 254;
 $arrData['bri'] = 254;
 
 $data = json_encode($arrData);
-echo $data;
+  $target="lights/7/state";
 
-//$url = "http://MYSITE/api/1234567890/lights/1/state";
 
+echo sendPut($data,$target);
+
+function sendPut($data, $target){
+//The URL that we want to send a PUT request to.
+$url = 'http://192.168.1.212/api/EwxOVEswAQTG-VfGrAYqbUHmGbCAm3sKDOOC-Uz2/'.$target;
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, $url);
 //curl_setopt($ch, CURLOPT_PORT, 8078);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -40,7 +42,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 
 $response = curl_exec($ch);
-echo $response; 
-
+return $response; 
+}
 
 ?>
